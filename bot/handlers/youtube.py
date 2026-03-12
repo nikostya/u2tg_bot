e60c77e7abcd0c3bed697692a237f0f58b28a620
Konstantin Nikolaev <nikostya@gmail.com>
2026-03-12 23:21:40 +0000

    Post link for download video instead post video in chat

--- bot/handlers/youtube.py
+++ bot/handlers/youtube.py
@@ -1,6 +1,8 @@
 from aiogram import Router
 from aiogram.types import Message, FSInputFile
 from bot.services.downloader import download_youtube
+from bot.services.upload_service import upload_video
+import os
 
 router = Router()
 
@@ -18,9 +20,17 @@ async def youtube_handler(message: Message):
     try:
         file_path = download_youtube(url)
 
-        video = FSInputFile(file_path)
+        await message.answer("Загружаю на сервер...")  # потом добавить размер файла
 
-        await message.answer_video(video)
+        link = upload_video(file_path)
+        os.remove(file_path)
+
+        await message.answer(
+        f'<a href="{link}">Скачать видео</a>',
+        parse_mode="HTML"
+        )
+
+        
 
     except Exception as e:
         await message.answer(f"Ошибка: {e}")
